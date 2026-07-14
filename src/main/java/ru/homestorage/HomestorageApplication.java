17ca25e8c764529966327e45085623b1f41580b2
Гревцов Максим Эдуардович <mgrevtsov@inpromconsult.ru>
2026-07-14 18:12:59 +0300

    Init Springboot project

---
+++ src/main/java/ru/homestorage/HomestorageApplication.java
@@ -0,0 +1,13 @@
+package ru.homestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomestorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomestorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/homestorage/HomestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
